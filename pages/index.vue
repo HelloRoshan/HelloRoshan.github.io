cf97452d597ef53b875e2ea9b7a5d5ecdf7752f0
Roshan <roshanbudhathoki02@gmail.com>
2020-01-17 01:00:27 +0545

    deploy test change

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
           Front<span class="red-text text-accent-3">-</span>End Developer<span class="red-text text-accent-3">.</span> 
         </h2>
         <h6 style="line-height:1.5;">
-          Hi, I'm Roshan! I am a web developer working with <span class="green-text text-accent-4">Vue.js</span>. I currently live in Kathmandu, Nepal. I am available for freelance jobs, just send me an email.
+          Hi, I'm Roshan! I am a Web Developer working with <span class="green-text text-accent-4">Vue.js</span>. I currently live in Kathmandu, Nepal. I am available for freelance jobs, just send me an email.
         </h6>
       </div>
     </section>
